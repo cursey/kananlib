7b3b6224768bd58c3b39a2432064fe7d107a5795
praydog <praydog@praydog.com>
2023-01-24 23:15:54 -0800

    Add ScopeGuard

---
+++ include/utility/ScopeGuard.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <functional>
+
+namespace utility{
+class ScopeGuard {
+public:
+    ScopeGuard() = delete;
+    ScopeGuard(std::function<void()> on_exit)
+        : m_on_exit{on_exit}
+    {
+    }
+
+    ~ScopeGuard() {
+        if (m_on_exit) {
+            m_on_exit();
+        }
+    }
+
+private:
+    std::function<void()> m_on_exit;
+};
+}
