d48bf423e144abc8fe9739d36db44006c99f7f2b
Sergey P <thirteenag@gmail.com>
2024-07-06 14:42:09 +0800

    Module: fix peb offset for x86

--- src/Module.cpp
+++ src/Module.cpp
@@ -297,7 +297,7 @@ namespace utility {
 #if defined(_M_X64)
         auto peb = (PEB*)__readgsqword(0x60);
 #else
-        auto peb = (PEB*)__readfsdword(0x60);
+        auto peb = (PEB*)__readfsdword(0x30);
 #endif
 
         if (peb == nullptr) {
