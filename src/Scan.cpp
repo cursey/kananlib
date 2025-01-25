7ceca3f80a1ec4f89b613afb3f5b41832766609e
praydog <praydog@praydog.com>
2025-01-25 14:01:37 -0800

    Scan: Update find_function_start_with_call with unwind variant

--- src/Scan.cpp
+++ src/Scan.cpp
@@ -1463,7 +1463,7 @@ namespace utility {
             return std::nullopt;
         }
 
-        for (auto func = find_function_start(middle); func.has_value(); func = find_function_start(*func - 1)) {
+        for (auto func = find_function_start_unwind(middle); func.has_value(); func = find_function_start_unwind(*func - 1)) {
             SPDLOG_INFO(" Checking if {:x} is a real function", *func);
 
             const auto ref = utility::scan_displacement_reference(module, *func, [&](uintptr_t ref) {
